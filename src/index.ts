a1968dea3fde66e26458018ca1d9dd9a31108a81
Vlad <vvllaadd1233345@gmail.co>
2023-04-21 23:28:04 +0200

    added express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import DietBotRoutes from './routes';
+import express from 'express';
 
 const dietBotRoutes = new DietBotRoutes();
 
@@ -10,3 +11,6 @@ dietBotRoutes.addedToChat();
 dietBotRoutes.joinedToChat();
 
 console.log('Started');
+
+const app = express();
+app.listen(process.env.PORT);
